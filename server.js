06b111761e247e9a9befcbf293e6cdd27597305e
Toffu <135154944+camen89@users.noreply.github.com>
2025-07-18 00:11:53 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const wss = new WebSocket.Server({ server: http });
 
 app.use(express.static('public'));
 
-// WebSocket接続・転送ロジック
 wss.on('connection', (ws) => {
-  console.log('New client connected. Total:', wss.clients.size);
-
-  ws.on('close', () => {
-    console.log('Client disconnected. Total:', wss.clients.size);
-  });
+  console.log('New client connected');
 
   ws.on('message', (data) => {
-    console.log('Received message:', typeof data === 'string' ? data : '[Binary Data]');
+    console.log('Received message:', typeof data, data.toString());
+
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
   });
 });
 
